1b2bbb23c72d0359561fa1aede60931572036eb8
yanshen1997 <yans@mail.ustc.edu.cn>
2022-01-31 20:26:24 +0800

    fix: 重构部分函数

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -98,31 +98,49 @@ func (c *Coordinator) monitorWorker(id int) {
 	}
 	select { // 这里后续仔细考察一下时序。20220126
 	case <-worker.ticker.C:
-		c.appandIdleTask(worker.currTask)
-		c.appandIdleWorker(worker)
-		worker.changeField(taskTimeout)
+		// c.appandIdleTask(worker.currTask)
+		c.syncChange(appandIdleTask, taskKey, worker.currTask)
+		// c.appandIdleWorker(worker)
+		c.syncChange(appandIdleWorker, workerKey, worker)
+		worker.syncChange(taskTimeout)
 	case <-worker.finishChan:
-		worker.changeField(finishTask)
+		worker.syncChange(finishTask)
 		c.finishWg.Done()
 	}
 }
 
+const (
+	workerKey      = "worker"
+	coordinatorKey = "coordinator"
+	taskKey        = "task"
+)
+
 // 修改worker字段
-func (w *worker) changeField(f func(*worker)) {
+func (w *worker) syncChange(f func(map[string]interface{}), is ...interface{}) {
 	w.lock.Lock()
 	defer w.lock.Unlock()
-	f(w)
+	vars := map[string]interface{}{}
+	vars[workerKey] = w
+	if len(is)&1 != 0 {
+		fmt.Println("warn:func (w *worker) syncChange: args.length % 2 != 0")
+	}
+	for i := 1; i < len(is); i += 2 {
+		vars[is[i-1].(string)] = is[i]
+	}
+	f(vars)
 }
 
 // worker完成任务后处理
-func finishTask(w *worker) {
+func finishTask(vars map[string]interface{}) {
+	w := vars[workerKey].(*worker)
 	w.currTask.state = taskState(completed)
 	w.currTask.currWorker = nil
 	w.currTask = nil
 	w.state = workerState(idle)
 }
 
-func taskTimeout(w *worker) {
+func taskTimeout(vars map[string]interface{}) {
+	w := vars[workerKey].(*worker)
 	curTask := w.currTask
 	if curTask == nil {
 		return
@@ -134,19 +152,27 @@ func taskTimeout(w *worker) {
 	w.state = workerState(idle)
 }
 
-func (c *Coordinator) changeField(f func(*Coordinator)) {
+func (c *Coordinator) syncChange(f func(map[string]interface{}), is ...interface{}) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
-	f(c)
+	vars := make(map[string]interface{})
+	vars[coordinatorKey] = c
+	if len(is)&1 != 0 {
+		fmt.Println("warn:func (c *Coordinator) syncChange: args.length %2 != 0")
+	}
+	for i := 1; i < len(is); i += 2 {
+		vars[is[i-1].(string)] = is[i]
+	}
+	f(vars)
 }
 
-func (c *Coordinator) appandIdleWorker(w *worker) {
+func appandIdleWorker(vars map[string]interface{}) {
+	c, w := vars[coordinatorKey].(*Coordinator), vars[workerKey].(*worker)
 	(*c.idleWorkers) = append((*c.idleWorkers), w)
 }
-func (c *Coordinator) appandIdleTask(t *task) {
-	c.lock.Lock()
+func appandIdleTask(vars map[string]interface{}) {
+	c, t := vars[coordinatorKey].(*Coordinator), vars[taskKey].(*task)
 	(*c.idleTasks) = append((*c.idleTasks), t)
-	c.lock.Unlock()
 }
 
 func (c *Coordinator) Finish(args *FinishArgs, reply *FinishReply) error {
@@ -213,20 +239,31 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{
-		lock:         sync.RWMutex{},
-		workerId:     0,
-		tempFilePath: os.Args[1:],
-		workers:      map[int]*worker{},
-		idleWorkers:  &([]*worker{}),
-		fileMapTasks: map[string]*task{},
-		idleTasks:    &([]*task{}),
-		finishWg:     sync.WaitGroup{},
-		finishChan:   make(chan struct{}),
-	}
+	c := Coordinator{}
 
 	// Your code here.
-	//随机添加不少于5个、不大于25个的任务
+	initCoordinator(&c)
+	testDemo(&c)
+	c.server()
+	return &c
+}
+
+// 初始化master
+func initCoordinator(c *Coordinator) {
+	c.lock = sync.RWMutex{}
+	c.workerId = 0
+	c.tempFilePath = os.Args[1:]
+	c.workers = map[int]*worker{}
+	c.idleWorkers = &([]*worker{})
+	c.fileMapTasks = map[string]*task{}
+	c.idleTasks = &([]*task{})
+	c.finishWg = sync.WaitGroup{}
+	c.finishChan = make(chan struct{})
+}
+
+//随机添加不少于5个、不大于25个的任务
+func testDemo(c *Coordinator) {
+	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 5+rand.Intn(20); i++ {
 		task := &task{
 			state:    taskState(idle),
@@ -242,6 +279,4 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 		c.finishWg.Wait()
 		c.finishChan <- struct{}{}
 	}()
-	c.server()
-	return &c
 }
